8abfebda546d89136522d5badc08fd2a11701466
malek2005 <malek2005@list.ru>
2021-03-14 16:41:00 +0200

    feat: solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) 
+{
+  for (let j = 0; j < bracketsConfig.length; j++) {
+      let item = bracketsConfig[j];
+      let open = item[0];
+      let close = item[1];
+      for (let i = 0; i < str.length; i++) {
+          if (str[i] == open && str[i + 1] == close) {
+              str = str.substr(0, i) + str.substr(i + 2);
+              i = -1;
+              j = -1;
+              continue;
+          }
+      }
+  }
+  return str.length === 0;
 }
